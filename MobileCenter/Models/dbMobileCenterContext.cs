c0a5e2cd50207159a94411e8d5048ea0ef402b4a
yeucon02vn <phiphuqn1@gmail.com>
2020-05-17 09:25:27 +0700

    feat : add migration

---
+++ MobileCenter/Models/dbMobileCenterContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MobileCenter.Models
+{
+    public class dbMobileCenterContext : DbContext
+    {
+        public dbMobileCenterContext() : base("name=dataMobileCenter")
+        {
+
+        }
+
+        //public DbSet<> Customer { get; set; }
+    }
+}
